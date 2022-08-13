323ef5c4156d676580f4f61a96442b06bc797e01
GaiBrutman <gai.brutman@gmail.com>
2022-08-13 13:22:38 +0300

    🎨 Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="feb",
+    version="0.0.1",
+    description="Feb Explores Binaries",
+    author="Gai Brutman",
+    packages=["feb"],
+)
